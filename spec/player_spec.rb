0af4b5b4e273ca50e5a0718b9ed6eaa93ef65068
sam fallowfield <sam.fallowfield1@gmail.com>
2022-02-28 12:02:47 +0000

    Create next move function

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,12 @@
 require_relative "../lib/player"
 
 describe Player do
-  # fit 'asks for input' do
-  #   board = ['X','','','','O','','','','']
-  #   player = Player.new
-  #   allow(player).to receive(:gets).and_return('Ting is the best', 'Ting is still the best', 'whatsaap', '0')
-  #   # expect(player).to receive(:gets)
-  #   player.input(board)
-  # end
+  it 'asks for input' do
+    player = Player.new
+    allow(player).to receive(:gets).and_return("hello")
+    expect(player).to receive(:gets)
+    player.input
+  end
 
   it "can find if the input matches the allowed characters" do
     #Arrange
@@ -35,19 +34,4 @@ describe Player do
       #Assert
       expect(result).to eq(expected_result)
   end
-
-  it "can see an index is occupied with an X or an O" do
-    #Arrange
-    player = Player.new
-    expected_result = true
-    board = ['X','','','','O','','','','']
-    player.move = 2
-
-    #Act
-    result = player.is_available?(board)
-
-    #Assert
-    expect(result).to eq(expected_result)
-  end
-
 end
