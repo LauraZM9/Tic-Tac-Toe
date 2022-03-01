4466d5e8a5eeec470cd923d300ed0988f3cc7329
sam fallowfield <sam.fallowfield1@gmail.com>
2022-03-01 17:56:15 +0000

    Added tests for the AI

---
+++ lib/computer.rb
@@ -0,0 +1,8 @@
+
+class Computer
+
+
+
+
+end
+
---
+++ spec/computer_spec.rb
@@ -0,0 +1,26 @@
+require_relative "../lib/computer"
+
+describe Computer do
+
+    it "will choose to play the winning spot if there is one" do
+    end
+
+    it "will not alter the grid when asked to choose a move" do
+    end
+
+    it " will choose the first winning move (instead of draw)" do
+    end 
+
+    it "will prevent opponent from winning if it can" do
+    end
+
+    it "will choose the first move that leads to a draw instead of losing thisgrid" do
+    end
+
+    it "will choose the first losing space if loss is unavoidable" do
+    end
+
+    it "if given an empty grid, all moves will be scored as a draw" do
+    end
+
+end
